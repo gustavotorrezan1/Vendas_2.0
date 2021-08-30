68a274d716a9372f97ce9a65236d9d6e36d64792
gustavotorrezan1 <74027638+gustavotorrezan1@users.noreply.github.com>
2021-08-30 20:53:55 -0300

    .
    .

--- src/util/Excluir.java
+++ src/util/Excluir.java
@@ -1,10 +1,9 @@
-
 package util;
 
+import controller.ControllerCliente;
 import controller.ControllerFornecedor;
 import controller.ControllerPessoa;
 import javax.swing.JOptionPane;
-import model.ModelPessoa;
 
 /**
  *
@@ -13,16 +12,20 @@ import model.ModelPessoa;
 public class Excluir {
 
     public static void deletar_fornecedor() {
-        ModelPessoa modelPessoa = new ModelPessoa();
         ControllerPessoa controllerPessoa = new ControllerPessoa();
         ControllerFornecedor controllerFornecedor = new ControllerFornecedor();
         int pesId = Integer.valueOf(JOptionPane.showInputDialog("Informe id do fornecedor"));
-        ModelPessoa ModelPessoa = new ModelPessoa();
         controllerPessoa.getPessoaController(pesId);
         controllerPessoa.excluirPessoaController(pesId);
         controllerFornecedor.excluirFornecedorController(pesId);
     }
-    public static void deletar_cliente(){
-        
+
+    public static void deletar_cliente() {
+        ControllerPessoa controllerPessoa = new ControllerPessoa();
+        ControllerCliente controllerCliente = new ControllerCliente();
+        int pesId = Integer.valueOf(JOptionPane.showInputDialog("Informe id do fornecedor"));
+        controllerPessoa.getPessoaController(pesId);
+        controllerPessoa.excluirPessoaController(pesId);
+        controllerCliente.excluirClienteController(pesId);
     }
 }
